0d274af47c81543aa5dcd845255ffe012080d5fc
Paul S. Chun <paul@sixofhearts.us>
2020-06-04 22:49:41 +0900

    initial build

---
+++ js/main.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",function(){const t=Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"),0);t.length>0&&t.forEach(function(t){t.addEventListener("click",function(){const e=t.dataset.target,n=document.getElementById(e);t.classList.toggle("is-active"),n.classList.toggle("is-active")})})});
